871a6f36a18c7ddf5fb62f260311b18b9b21e537
Josh <hi@jossh.dev>
2025-04-29 22:32:27 -0400

    style: offset underline

--- src/lib/ui/typography/FancyLink.svelte
+++ src/lib/ui/typography/FancyLink.svelte
@@ -7,7 +7,10 @@
 </script>
 
 <a
-    class={merge('inline-flex items-center gap-md focus-within:underline hover:underline', clazz)}
+    class={merge(
+        'inline-flex items-center gap-md underline-offset-4 focus-within:underline hover:underline',
+        clazz,
+    )}
     href={link.href}
     aria-label={link.label}
     target={!link.internal ? '_blank' : ''}
