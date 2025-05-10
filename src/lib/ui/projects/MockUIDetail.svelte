f275f48d2b1747149b0d5dc9fc04742cd81b51da
Josh <hi@jossh.dev>
2025-05-10 17:17:08 -0400

    fix: clean up mock ui detail styles

--- src/lib/ui/projects/MockUIDetail.svelte
+++ src/lib/ui/projects/MockUIDetail.svelte
@@ -1,28 +1,31 @@
 <div
-    class="pointer-events-none absolute top-3xl flex flex-wrap items-center justify-center gap-xl *:shrink-0 *:grow-0 max-sm:brightness-50 sm:center-y sm:right-2xl sm:w-2/5"
+    class="absolute top-3xl flex flex-wrap items-center justify-center gap-md px-lg max-md:brightness-75 max-md:*:scale-75 sm:top-4xl md:center-y md:left-1/2 md:w-1/2 md:gap-xl md:px-2xl"
 >
     <img
         class="animate-floating w-40"
         src="/img/proj_mock-ui_detail-1.webp"
         alt="Mock UI detail 1"
     />
+
     <img
         class="animate-floating w-28"
         style="animation-delay: 500ms"
         src="/img/proj_mock-ui_detail-2.webp"
         alt="Mock UI detail 2"
     />
+
     <img
         class="animate-floating w-42"
         style="animation-delay: 1000ms"
         src="/img/proj_mock-ui_detail-3.webp"
         alt="Mock UI detail 3"
     />
+
     <img
         class="animate-floating w-64"
         style="animation-delay: 1500ms"
         src="/img/proj_mock-ui_detail-6.webp"
-        alt=""
+        alt="Mock UI detail 6"
     />
 </div>
 
