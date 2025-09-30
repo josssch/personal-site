fa00ef66fc9fde3ff42e292bac7bc98d05ae2a02
Josh <hi@jossh.dev>
2025-09-30 14:43:53 -0400

    refactor: remove Book Me
    I somehow just discovered cal.com, woops

--- src/lib/ui/projects/BookMeDetail.svelte
+++
@@ -1,7 +0,0 @@
-<div class="absolute h-full w-full md:left-1/2 md:w-1/2">
-    <img
-        class="absolute top-1/8 center-x aspect-[5/2] w-1/2 rounded-2xl bg-theme-bg-1 object-cover text-transparent shadow-lg max-md:brightness-75 max-sm:w-3/4 md:center-y md:w-3/4"
-        src="/img/proj_book-me_banner.jpg"
-        alt="Book Me Preview"
-    />
-</div>
