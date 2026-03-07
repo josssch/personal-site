15debb66373775d4df1a1329c251f1ef80171666
Josh <hi@jossh.dev>
2026-03-07 13:44:55 -0500

    feat: use the slug as the hash over index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,29 +22,36 @@
         }
     }
 
-    // get the initial index out of the window hash (minus one)
-    const initialIndex = window.location.hash ? Number(window.location.hash.slice(1)) - 1 : 0
-
     const allProjects = Object.values(projects)
+
+    // get the initial index out of the window hash (minus one)
+    const initialHash = window.location.hash ? window.location.hash.slice(1) : null
+    const initialIndex = initialHash ? allProjects.findIndex(p => p.slug === initialHash) : -1
 
     // update the window hash every time the index changes
     // fixme: makes it so users can't edit the hash
-    let index = $state(initialIndex)
+    let index = $state(initialIndex >= 0 && initialIndex < allProjects.length ? initialIndex : 0)
+    const project = $derived(allProjects[index])
+
     $effect(() =>
         replaceState(
-            `#${index + 1}`,
+            project ? `#${project.slug}` : '',
             untrack(() => page.state),
         ),
     )
 
     afterNavigate(() => {
+        if (!project) return
+
+        // this is to let the individual project screen know there was a transition
+        // and should display a back button
         replaceState('', {
-            from: `/#${index + 1}`,
+            from: `/#${project.slug}`,
         })
     })
 
-    const projectThemeBg = $derived(allProjects[index]?.theme.backgroundColor)
-    const projectThemeText = $derived(allProjects[index]?.theme.textColor)
+    const projectThemeBg = $derived(project?.theme.backgroundColor)
+    const projectThemeText = $derived(project?.theme.textColor)
 
     $effect(() => {
         document.body.classList.toggle('transition-colors', true)
